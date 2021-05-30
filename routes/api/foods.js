7ae6ad961609b02548406be04d45364f25e7ddd4
RomanOmelianenko <r_omelianenko@ukr.net>
2021-05-30 13:40:13 +0300

    fixed dily calories and products

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const calculator = require('../../src/controllers/controllersProducts')
+// const calculator = require('../../src/controllers/controllersProducts')
 const controllerProducts = require('../../src/controllers/controllersProducts')
-const { validateCalc } = require('../../src/validation/calculatorValidate')
+// const { validateCalc } = require('../../src/validation/calculatorValidate')
 
 router
-  .get('/', controllerProducts.getProducts)
-  .get('/calculator', validateCalc, calculator.getDailyCalories)
+  .get('/', /* validateCalc, */ controllerProducts.getCaloriesNotRecProduct)
+  // .get('/calculator', validateCalc, calculator.getDailyCalories)
   .get('/foods', controllerProducts.getProductsByQuery)
 
 module.exports = router
